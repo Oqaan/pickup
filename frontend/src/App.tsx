dcf3bb84d16939263f2d5787efa181fe1c5cabb4
Oqaan <okan_altun@hotmail.com>
2026-08-01 15:52:05 +0200

    feat: add home page with fuzzy search

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SeriesPage from "./pages/SeriesPage";
+import HomePage from "./pages/HomePage";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/anime/:slug" element={<SeriesPage />} />
+        <Route path="/" element={<HomePage />} />
       </Routes>
     </BrowserRouter>
   );
