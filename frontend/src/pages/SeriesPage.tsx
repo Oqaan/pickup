90938477932400984f90ee3eda84ae062815ba20
Okan <okan_altun@hotmail.com>
2026-08-03 11:10:17 +0200

    fix: remove unused import (#13)

--- frontend/src/pages/SeriesPage.tsx
+++ frontend/src/pages/SeriesPage.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Link, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { fetchSeriesDetail } from "../api";
 import type { SeriesDetail } from "../types";
 
