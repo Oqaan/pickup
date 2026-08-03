5e3b2209db2e179623f4c45ede40ea1825627101
Oqaan <okan_altun@hotmail.com>
2026-08-03 16:40:49 +0200

    chore: add ci workflow with testcontainers

--- backend/src/test/java/dev/okanaltun/pickup/PickupApplicationTests.java
+++ backend/src/test/java/dev/okanaltun/pickup/PickupApplicationTests.java
@@ -2,12 +2,13 @@ package dev.okanaltun.pickup;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
+@Import(TestcontainersConfiguration.class)
 @SpringBootTest
 class PickupApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
