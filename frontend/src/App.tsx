6c6090adbff14e21e736935487b02125e2ed0789
Okan <okan_altun@hotmail.com>
2026-08-05 15:30:33 +0200

    feat: add a 404 page for unknown routes (#27)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import SeriesPage from "./pages/SeriesPage";
 import HomePage from "./pages/HomePage";
 import Layout from "./components/Layout";
 import AboutPage from "./pages/AboutPage";
+import NotFoundPage from "./pages/NotFoundPage";
 
 export default function App() {
   return (
@@ -12,6 +13,7 @@ export default function App() {
           <Route path="/anime/:slug" element={<SeriesPage />} />
           <Route path="/" element={<HomePage />} />
           <Route path="/about" element={<AboutPage />} />
+          <Route path="*" element={<NotFoundPage />} />
         </Routes>
       </Layout>
     </BrowserRouter>
