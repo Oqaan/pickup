efe5c331af20a95ea8411f7deb137f25c708f6f6
Okan <okan_altun@hotmail.com>
2026-08-09 09:18:36 +0200

    fix: scroll to top on forward navigation (#47)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,10 +4,12 @@ import HomePage from "./pages/HomePage";
 import Layout from "./components/Layout";
 import AboutPage from "./pages/AboutPage";
 import NotFoundPage from "./pages/NotFoundPage";
+import ScrollToTop from "./ScrollToTop";
 
 export default function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <Layout>
         <Routes>
           <Route path="/anime/:slug" element={<SeriesPage />} />
